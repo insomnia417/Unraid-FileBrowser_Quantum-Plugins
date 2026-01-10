53f749c6e0e03f1391de85f3bd4d692ac1b792aa
insomnia417 <47959853+insomnia417@users.noreply.github.com>
2026-01-10 11:29:21 +0800

    Update updatefilebrowser_quantum.sh

--- source/filebrowser_quantum/usr/local/emhttp/plugins/filebrowser_quantum/updatefilebrowser_quantum.sh
+++ source/filebrowser_quantum/usr/local/emhttp/plugins/filebrowser_quantum/updatefilebrowser_quantum.sh
@@ -1,50 +1,25 @@
 #!/bin/bash
+NAME="filebrowser_quantum"
+INSTALL_DIR="/boot/config/plugins/$NAME/install"
+# 定义下载地址（指向官方最新编译的二进制）
+URL="https://github.com/gtsteffaniak/filebrowser/releases/latest/download/linux-amd64-filebrowser"
 
-if [ "$1" = "2" ]; then
-   fburl="https://github.com/gtsteffaniak/filebrowser/releases/latest/download/linux-amd64-filebrowser"
+# 1. 停止当前服务
+/usr/local/emhttp/plugins/$NAME/webuiScript.sh "false"
+
+# 2. 下载新文件
+echo "Downloading newest binary from GitHub..."
+curl --connect-timeout 15 --retry 3 -L -o "$INSTALL_DIR/$NAME.new" "$URL"
+
+# 3. 校验并替换
+if [ $? -eq 0 ] && [ -s "$INSTALL_DIR/$NAME.new" ]; then
+    mv "$INSTALL_DIR/$NAME.new" "$INSTALL_DIR/$NAME"
+    cp "$INSTALL_DIR/$NAME" "/usr/sbin/$NAME-orig"
+    chmod 755 "/usr/sbin/$NAME-orig"
+    echo "Update completed successfully."
 else
-   fburl="https://github.com/gtsteffaniak/filebrowser/releases/download/v1.1.6-beta/linux-amd64-filebrowser"
-fi;
+    echo "Update failed: Download error or file empty."
+fi
 
-version=`filebrowser_quantum-orig version | head -n 1`
-  echo "-------------------------------------------------------------------"
-  echo "Validating connection to internet"
-  echo "-------------------------------------------------------------------"
-ping -q -c2 github.com >/dev/null || ping -q -c2 1.1.1.1 >/dev/null || ping -q -c2 8.8.8.8 >/dev/null
-if [ $? -eq 0 ]
-then
-  echo "-------------------------------------------------------------------"
-  echo "Updating filebrowser_quantum"
-  echo "-------------------------------------------------------------------"
-  curl --connect-timeout 15 --retry 3 --retry-delay 2 --retry-max-time 30 -o /boot/config/plugins/filebrowser_quantum/install/filebrowser_quantum.zip $fburl
-  unzip -o -j "/boot/config/plugins/filebrowser_quantum/install/filebrowser_quantum.zip" "*/filebrowser_quantum" -d "/boot/config/plugins/filebrowser_quantum/install"
-  rm -f /boot/config/plugins/filebrowser_quantum/install/*.zip
-  cp /boot/config/plugins/filebrowser_quantum/install/filebrowser_quantum /usr/sbin/filebrowser_quantum-orig.new
-  mv /usr/sbin/filebrowser_quantum-orig.new /usr/sbin/filebrowser_quantum-orig
-  chown root:root /usr/sbin/filebrowser_quantum-orig
-  chmod 755 /usr/sbin/filebrowser_quantum-orig
-else
-  echo ""
-  echo "-------------------------------------------------------------------"
-  echo "<font color='red'> Connection error - Can't fetch new version </font>"
-  echo "-------------------------------------------------------------------"
-  echo ""
-  exit 1
-fi;
-
-current_version=`filebrowser_quantum-orig version | head -n 1`
-
-if [[ $version = $current_version ]]; then
-  echo ""
-  echo "-------------------------------------------------------------------"
-  echo "<font color='red'> Update failed - Please try again </font>"
-  echo "-------------------------------------------------------------------"
-  echo ""
-  exit 1
-fi;
-
-echo ""
-echo "-------------------------------------------------------------------"
-echo " filebrowser_quantum has been successfully updated "
-echo "-------------------------------------------------------------------"
-echo ""
+# 4. 重新启动服务
+/usr/local/emhttp/plugins/$NAME/webuiScript.sh "true"
