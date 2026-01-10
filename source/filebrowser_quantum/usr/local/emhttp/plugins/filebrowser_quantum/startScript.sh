805dd444fde05f24df1b0cc4f95efdcbed00dbb4
insomnia417 <47959853+insomnia417@users.noreply.github.com>
2026-01-10 11:48:26 +0800

    Update startScript.sh

--- source/filebrowser_quantum/usr/local/emhttp/plugins/filebrowser_quantum/startScript.sh
+++ source/filebrowser_quantum/usr/local/emhttp/plugins/filebrowser_quantum/startScript.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 NAME="filebrowser_quantum"
-
-# 修复可能的权限丢失
+# 权限修复
 chmod 755 /usr/local/emhttp/plugins/$NAME/*.sh
 if [ -f "/usr/sbin/$NAME-orig" ]; then
     chmod 755 "/usr/sbin/$NAME-orig"
 fi
-
 # 启动
 /usr/local/emhttp/plugins/$NAME/webuiScript.sh "true"
