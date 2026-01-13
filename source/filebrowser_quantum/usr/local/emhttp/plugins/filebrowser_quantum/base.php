420dac93e24e1b5e221ee76d6642ec2c2adb0030
insomnia417 <47959853+insomnia417@users.noreply.github.com>
2026-01-13 20:16:30 +0800

    Update base.php

--- source/filebrowser_quantum/usr/local/emhttp/plugins/filebrowser_quantum/base.php
+++ source/filebrowser_quantum/usr/local/emhttp/plugins/filebrowser_quantum/base.php
@@ -2,11 +2,14 @@
 // 插件路径
 $PLG_PATH = "/boot/config/plugins/filebrowser_quantum";
 // config.yaml路径
-$CONFIG_FILE = "$PLG_PATH/config.yaml";
+$CONFIG_YAML = "$PLG_PATH/config.yaml";
 // 插件附属配置文件
 $SETTINGS_FILE = "$PLG_PATH/settings.cfg";
 // 插件包下载路径
 $INSTALL_PATH = "$PLG_PATH/install";
+// 其他变量
+$LATEST_FILE = "$INSTALL_PATH/latest";
+$BETA_MARKER = "$INSTALL_PATH/beta";
 
 // 统一的日志提取逻辑
 function getLogPath($configFile) {
