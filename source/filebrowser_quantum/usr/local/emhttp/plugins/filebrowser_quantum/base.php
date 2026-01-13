db333ae1c2aa510fbe492ec625742e2072d6de78
insomnia417 <47959853+insomnia417@users.noreply.github.com>
2026-01-13 19:51:26 +0800

    Update base.php

--- source/filebrowser_quantum/usr/local/emhttp/plugins/filebrowser_quantum/base.php
+++ source/filebrowser_quantum/usr/local/emhttp/plugins/filebrowser_quantum/base.php
@@ -1,8 +1,11 @@
 <?PHP
-// v1.0.0 - 共享路径定义
+// 插件路径
 $PLG_PATH = "/boot/config/plugins/filebrowser_quantum";
+// config.yaml路径
 $CONFIG_FILE = "$PLG_PATH/config.yaml";
+// 插件附属配置文件
 $SETTINGS_FILE = "$PLG_PATH/settings.cfg";
+// 插件包下载路径
 $INSTALL_PATH = "$PLG_PATH/install";
 
 // 统一的日志提取逻辑
